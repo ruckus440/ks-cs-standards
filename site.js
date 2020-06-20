70164a2438e0a86d700ab445b24f51567a109000
Mike Ruckert <mwr3333@ksu.edu>
2020-06-20 22:18:49 +0000

    Initial commit

---
+++ site.js
@@ -0,0 +1,57 @@
+/** site.js 
+ * You should add your JavaScript code to this file.  
+ * See the assignment description in Guide for what
+ * your code needs to accomplish.
+ */
+
+document.body.onload = addElement;
+
+function addElement () {
+
+    standards.forEach(element => {
+        var standardDiv = document.createElement("div");
+        standardDiv.className = "stdItem";
+ 
+        Object.keys(element).forEach(key => {
+            var newDiv = document.createElement('div')
+            newDiv.className = key
+            newDiv.innerHTML = element[key]
+            if (key == 'description' || key == 'subconcept' || key == 'practices'){
+                newDiv.classList.add("hideable");
+            }
+            standardDiv.appendChild(newDiv);
+        })
+
+        var currentDiv = document.getElementById("stdDiv");
+        currentDiv.appendChild(standardDiv);
+
+        var button = document.createElement("button");
+        button.className = "collapsible";
+        button.onclick = function() { ToggleShow(button); };
+
+        standardDiv.appendChild(button);        
+    });
+
+    getButtons();
+}
+
+function ToggleShow(button) {
+
+    var parent = button.parentElement;
+    var divs = parent.querySelectorAll('.hideable');
+
+    divs.forEach(function(item) {
+        item.classList.toggle('show');
+    })
+}
+
+function getButtons() {
+    
+    var buttons = document.querySelectorAll('.collapsible');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });        
+    });
+}
